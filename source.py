4a7ad449fac8df4a84b552b8a6907b33dffd9ed8
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-03-09 20:01:53 +0530

    Create source.py

---
+++ source.py
@@ -0,0 +1,33 @@
+import face_recognition
+
+# Store the known faces and their names
+known_faces = {
+    "John": face_recognition.load_image_file("john.jpg"),
+    "Sarah": face_recognition.load_image_file("sarah.jpg"),
+    "Mark": face_recognition.load_image_file("mark.jpg")
+}
+
+# Encode the known faces using face_recognition library
+known_encodings = {}
+for name, face in known_faces.items():
+    known_encodings[name] = face_recognition.face_encodings(face)[0]
+
+# Capture the image of the person who wants to enter
+unknown_image = face_recognition.load_image_file("unknown.jpg")
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+# Compare the unknown face to the known faces
+results = face_recognition.compare_faces(list(known_encodings.values()), unknown_encoding)
+
+# Get the names of the known faces
+names = list(known_encodings.keys())
+
+# Display the results
+if True in results:
+    # Get the name of the matched face
+    match_index = results.index(True)
+    name = names[match_index]
+    print(f"Welcome, {name}!")
+else:
+    # Display error message if the face doesn't match any of the known faces
+    print("Sorry, your identity cannot be verified.")
